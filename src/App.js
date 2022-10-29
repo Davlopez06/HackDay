55a0a1966869a80dbbca731f5b9b56f80c3b51e5
Davlopez06 <davidlopezvaldes@gmail.com>
2022-10-29 16:40:53 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import Agentes from './pages/Agentes.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Activo from './Componentes/Activo';
 import Navbar from './Componentes/Navbar';
+import Home from './Componentes/Home';
 
 
 const App = () => {
   return (
       <React.Fragment>
         <Routes>
-          <Route exact path="/" element={<LandingPage/>}/> 
+          <Route exact path="/" element={<Home/>}/> 
           <Route exact path="/Agentes" element={<Agentes/>}/> 
           <Route exact path='/Activos' element={<Activo/>}/>
           <Route exact path='/Navbar' element={<Navbar/>}/>
