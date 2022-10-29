24f9041f1d8188248237283c65718bed8db8b002
Davlopez06 <davidlopezvaldes@gmail.com>
2022-10-29 15:41:03 -0500

    Activo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import './App.css';
 import LandingPage from './pages/LandingPage.jsx';
 import Agentes from './pages/Agentes.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Activo from './Componentes/Activo';
 
 
 const App = () => {
   return (
-      <React.Fragment>     
-          <Route exact path="/" component={LandingPage}/> 
-          <Route exact path="/Agentes" component={Agentes}/> 
+      <React.Fragment>
+        <Routes>
+          <Route exact path="/" element={<LandingPage/>}/> 
+          <Route exact path="/Agentes" element={<Agentes/>}/> 
+          <Route exact path='/Activos' element={<Activo/>}/>
+        </Routes>
       </React.Fragment>
   );
 }
